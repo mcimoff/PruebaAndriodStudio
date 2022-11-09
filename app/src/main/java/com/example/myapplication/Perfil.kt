245aca65027aed86c9315c7381e2cf0b8ffbdb86
ShanonSamora <shanonsamora@gmail.com>
2022-11-09 20:20:27 -0300

    Autenticacion Firebase

--- app/src/main/java/com/example/myapplication/Perfil.kt
+++ app/src/main/java/com/example/myapplication/Perfil.kt
@@ -1,20 +1,17 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.PreferenceManager
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class Perfil : Fragment() {
+class Perfil : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.user_preferences, rootKey)
+    }
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -24,27 +21,27 @@ class Perfil : Fragment() {
 
     }
 
-    override fun onCreateView(
+    /*override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_perfil, container, false)
-    }
+    }*/
 
-    override fun onStart() {
+    /*override fun onStart() {
         super.onStart()
 
         val prefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
 
         Log.d("Preferencias", prefs.getBoolean("night_mode", false).toString() + " - night mode")
         Log.d("Preferencias", prefs.getString("edit_email", "email default") + " - edit email")
-    }
+    }*/
 
-    class SettingsFragment : PreferenceFragmentCompat() {
+    /*class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.user_preferences, rootKey)
         }
 
-    }
+    }*/
 }
