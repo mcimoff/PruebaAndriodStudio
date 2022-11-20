a353202f0245b7e9298a2ddd5bb65bc7590493de
ShanonSamora <shanonsamora@gmail.com>
2022-11-20 15:49:45 -0300

    Reset password en login, modo noche en ajustes y reset password en ajustes (a corregir)

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.PreferenceManager
+import android.view.Menu
+import android.view.MenuItem
+import com.example.myapplication.activities.SettingsActivity
 
 enum class ProviderType {
     BASIC,
@@ -19,6 +25,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val darkModeValues = resources.getStringArray(R.array.dark_mode_values)
+        // The apps theme is decided depending upon the saved preferences on app startup
+        when (PreferenceManager.getDefaultSharedPreferences(this)
+            .getString(getString(R.string.dark_mode), getString(R.string.dark_mode_def_value))) {
+            darkModeValues[0] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            darkModeValues[1] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            darkModeValues[2] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            darkModeValues[3] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+        }
         setContentView(R.layout.activity_main)
 
         navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
@@ -36,4 +51,17 @@ class MainActivity : AppCompatActivity() {
         val bundle: Bundle? = intent.extras
         return bundle?.getString("provider")
     }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+        if (item.itemId == R.id.settings) {
+            val intent = Intent(this, SettingsActivity::class.java).apply {
+                putExtra("email", getEmail())
+            }
+            startActivity(intent)
+            true
+        } else super.onOptionsItemSelected(item)
 }
